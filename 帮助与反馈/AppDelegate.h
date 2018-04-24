5ea7d1846b5e9165736cec31e3ea86963129c6b4
董向坤 <1043643016@qq.com>
2018-04-24 10:06:49 +0800

    create project

---
+++ 帮助与反馈/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  帮助与反馈
+//
+//  Created by Kevin on 2017/8/25.
+//  Copyright © 2017年 Dongxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 帮助与反馈/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  帮助与反馈
+//
+//  Created by Kevin on 2017/8/25.
+//  Copyright © 2017年 Dongxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
